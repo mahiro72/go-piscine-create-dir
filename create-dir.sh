89aaabcd4a7f677d11c517b9d0f775f5dca95379
mahiro <mahiro.yoshie@gmail.com>
2022-05-05 04:40:28 +0900

    script add

---
+++ create-dir.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# 引数の数の確認
+if [ $# -ne 1 ]; then
+    echo ""
+    echo "指定された引数は$#個です"
+    echo "実行するには1個の引数が必要です"
+    echo ""
+    exit 1
+fi
+
+# 数字かどうか確認
+if expr "$1" : "[0-9]*$"; then
+    echo ""
+else
+    echo ""
+    echo "数字を入力してください"
+    echo ""
+    exit 1
+fi
+
+# 数字の値の確認
+if [ 0 -gt "$1" ] || [ "$1" -gt 20 ]; then
+    echo ""
+    echo "指定された数字が不正です"
+    echo "0以上20以下の数字を指定してください"
+    echo ""
+    exit 1
+
+fi
+
+echo ""
+echo "ex00 ~ ex0$1 を作成します..."
+echo ""
+
+cd ../
+
+i=0
+while [ $i -le "$1" ]; do
+    mkdir "ex0$i" && mkdir "ex0$i/vendor" && mkdir "ex0$i/vendor/piscine" && \
+    mkdir "ex0$i/vendor/ft" && touch "ex0$i/main.go" && cp "./go_piscine-create_dir/src/printrune.go" "ex0$i/vendor/ft/printrune.go"
+    i=$(( i + 1 ))
+done
+
+echo ""
+echo "作成が完了しました!"
+echo ""
+
+exit 0
---
+++ delete-dir.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo ""
+echo "exXX ディレクトリをすべて削除します"
+echo ""
+echo "現在のコードも削除されます"
+echo ""
+echo "本当によろしいですか？ [Y/n]: "
+echo ""
+
+read -r ANS
+
+case $ANS in
+  "" | [Yy]* )
+    echo ""
+    echo "削除を開始します..."
+    echo ""
+    rm -rf ../ex*
+    echo "削除が完了しました！"
+	echo ""
+    ;;
+  * )
+    # ここに「No」の時の処理を書く
+    echo ""
+    echo "削除を実行しません..."
+    echo ""
+    exit 0
+    ;;
+esac
